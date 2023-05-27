c9c9059fd3110ea0f558c13bb18b2bf970e6c735
sergio-nezhigay <58699786+sergio-nezhigay@users.noreply.github.com>
2023-05-27 13:06:13 +0000

    Deploying to gh-pages from @ sergio-nezhigay/goit-react-hw-08-phonebook@2677c3837234013f111b8f64946de7bfba9b5dc4 🚀

---
+++ static/js/679.8332cd88.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[679],{3679:function(e,t,r){r.r(t),r.d(t,{default:function(){return c}});r(2791);var n=r(3329);function c(){return(0,n.jsx)(n.Fragment,{children:(0,n.jsx)("h1",{children:"Home "})})}}}]);
+//# sourceMappingURL=679.8332cd88.chunk.js.map
